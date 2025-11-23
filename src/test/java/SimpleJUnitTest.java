9f232098f7cab4c6cd8afea11bdbb6e6c19f1830
Maksim Pozniak <abc-1765@ya.ru>
2025-11-23 20:09:04 +0300

    added Junit 5

---
+++ src/test/java/SimpleJUnitTest.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SimpleJUnitTest {
+
+    int result = getResult();
+
+    @AfterEach
+    void AfterEach() {
+        System.out.println("###      AfterEach\n");
+        result = 0;
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("\n###      BeforeAll\n");
+    }
+
+    @BeforeEach
+            void beforeEach() {
+        System.out.println("###      BeforeEach");
+        result = getResult();
+    }
+
+
+    private int getResult() {
+        return  3;
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("###      firstTest");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("###      secondTest");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void thirdTest() {
+        System.out.println("###      thirdTest");
+        Assertions.assertTrue( result > 2);
+    }
+
+
+}
